68411badd6071c8fc1a7baa56003d9fe260f2915
nbogie <neillbogie@googlemail.com>
2023-06-24 11:20:31 +0100

    formatting only

--- src/body.ts
+++ src/body.ts
@@ -1,6 +1,6 @@
 import p5 from "p5";
-import Board from "./board"
-import Cell from "./cell"
+import Board from "./board";
+import Cell from "./cell";
 
 interface CellCoordinate {
     x: number;
@@ -10,7 +10,8 @@ interface CellCoordinate {
 /**
  * Base class for the body components of the snake
  */
-class Body{
+
+class Body {
     _id: number;
     _position: Cell;
     _pre_tail: Tail | Head;
@@ -18,7 +19,7 @@ class Body{
     _gameBoard: Board;
     _previousPosition: Cell;
 
-    constructor(_positionCoordinate: CellCoordinate, _gameBoard: Board){
+    constructor(_positionCoordinate: CellCoordinate, _gameBoard: Board) {
         //console.log(_positionCoordinate, _gameBoard);
         this._gameBoard = _gameBoard;
         this._positionCoordinate = _positionCoordinate;
@@ -27,64 +28,60 @@ class Body{
         this._previousPosition = this._position;
     }
 
-
     /**
-     * 
+     *
      * @param new_position the position to which the body object should be moved to on the next iteration
      */
-    updatePosition(new_position){
+    updatePosition(new_position) {
         this.previousPosition = this.position;
         this.position = new_position;
     }
 
-
-
     // ------------------------------------------- GETTER METHODS ------------------------------------------- \\
-    get position(){
+    get position() {
         return this._position;
     }
-    get positionCoordinate(){
+    get positionCoordinate() {
         return this._positionCoordinate;
-    }    
-    get previousPosition(){
+    }
+    get previousPosition() {
         return this._previousPosition;
     }
     // ------------------------------------------- SETTER METHODS ------------------------------------------- \\
-    set position(new_position: Cell){
+    set position(new_position: Cell) {
         this._position = new_position;
     }
-    set previousPosition(current_position: Cell){
+    set previousPosition(current_position: Cell) {
         this._previousPosition = current_position;
     }
-    set positionCoordinate(new_coordinate: CellCoordinate){
+    set positionCoordinate(new_coordinate: CellCoordinate) {
         this._positionCoordinate = new_coordinate;
     }
 }
 
-
 /**
  * I no longer remember why this originally needed to be its own class
  */
-class Head extends Body{
-    constructor(_startCoordinate: CellCoordinate, _gameBoard: Board){
+class Head extends Body {
+    constructor(_startCoordinate: CellCoordinate, _gameBoard: Board) {
         super(_startCoordinate, _gameBoard);
     }
-
-
 }
 
-
 /**
- * Extends the body object to allow the storing of 
+ * Extends the body object to allow the storing of
  */
-class Tail extends Body{
+class Tail extends Body {
     _pre_tail: Tail | Head;
-    constructor(_id: number, _position: Cell, _pre_tail: (Head | Tail), _gameBoard: Board){
+    constructor(
+        _id: number,
+        _position: Cell,
+        _pre_tail: Head | Tail,
+        _gameBoard: Board
+    ) {
         super(_position.coordinate, _gameBoard);
         this._pre_tail = _pre_tail;
     }
-
-
 }
 
-export {Head, Body, Tail}
+export { Head, Body, Tail };
